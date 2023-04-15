a61ac8c1da67cfeda2550cf5879e1aaba88a5963
Avala233 <avalianiii13@gmail.com>
2023-04-15 17:27:20 +0400

    first commit

---
+++ day 1/lesson1.py
@@ -0,0 +1,31 @@
+
+verb1 = (" me var")
+name = "davit"
+
+surname ="avaliani"
+age = 23
+ageunit =("wlis")
+
+verb2 =("var")
+height = 180
+heightunit = ("santimetri")
+conj="da"
+weight = 92
+weightunit= "kilogrami"
+
+knows_programing = False
+if knows_programing== False:
+    print(" Welcome to club!")
+
+
+
+
+
+
+     
+
+
+
+
+
+print(verb1+" "+ name +" "+surname+" "+str(age) +" "+ ageunit+" "+verb2+" "+str(height)+" "+heightunit+" "+conj+" "+str(weight)+" "+weightunit)
