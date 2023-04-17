4869066f2e5058a1b764b23ff382e8564ca31c0d
Avala233 <avalianiii13@gmail.com>
2023-04-17 16:57:45 +0400

    day 2

---
+++ day 2/lesson2.py
@@ -0,0 +1,58 @@
+# name = "daviti"
+
+# print(len(name))  #len-Lengh (6) აუცილებლად ჩაითვლება სფეისიც
+
+# my_sentence ="""გამარჯობა, შეგახსენებთ რომ სწავლა დაიწყება 19 სექტემბერს, გთხოვთ არ 
+# გააცდინოთ"""
+# print(my_sentence)
+# """ .... """ წინადადებისთვის რომელიც არ ეტევა ერთ ხაზში
+
+#knows_programing =True
+
+# if 10>5:        #if True:     
+#     print("hello")    ტაბის საშუალებით მოხდა ინდენდტაცია (indentation)
+
+# #if 10<5:      #if False:
+#     print("hello") 
+
+#full_name =("davit avaliani") 
+
+#ყველა სტრინგი შეგვიძლია მივიჩნიოთ სიად
+
+# print(len(full_name))
+# print(full_name[0])   # [] position=index ვინაიდან ნულიდან იწყება ათვლა d იქნება სიმბოლო რომელსაც ამოპრინტავს
+
+#print("d" not in full_name)    # in da not in - დებულების სიზუსტის მიხედვით True an False 
+
+# full_name =("davit avaliani") 
+
+# print(full_name[2:7])  #დაპრინტავს მეორედან მეშვიდემდე  (vit a) ამ შემთხვევაში
+# print(full_name[2:7:2]) #დაპრინტავს მეორედან მეშვიდემდე ყოველ მეორეს (vta)  #start:finish:step
+
+# print(full_name[5:]) #ამოპრინტავს მეხუთედან ბოლომდე ( avaliani)
+# print(full_name[:5]) #ბოლოდან მეხუთემდე (davit )
+
+# full_name =("davit avaliani") 
+#print(full_name[-1]) #დაპრინტავს ბოლო სიმბოლოს (i)
+#print(full_name[-1]+"abc") #დაპრინტავს ბოლო სიმბოლოს+ფრჩხილებში რაც იქნება
+
+# full_name =("davit avaliani")
+# print(full_name[-1:-4:-1]) #დაპრინტავს ბოლოდან პირველიდან ბოლოდან მეოთხემდე (ina)
+
+#სტრინგებს აქვთ მეთოდები
+  #1)სტრინგ.upper()  #სტრინგს გამოვუძახოთ მეთოდი ამ შემთხვევაში upper
+  #2)len(სტრინგ)
+
+#name ="daviti"
+#print(name.upper())  დაიწერება(DAVITI)
+
+# name ="  daviti  "
+# print(name.strip()) .strip-ი აშორებს სფეისს მივიღებთ (daviti)
+
+# name ="  daviti  "
+# print(name.replace(" "," #")) # .replace ცვლის სიმბოლოს ჩვენი არჩეული სიმბოლოთი ამ შემთხვევაში სფეისები შეიცვლება ძიეზით (# #daviti# #)
+
+
+
+
+
