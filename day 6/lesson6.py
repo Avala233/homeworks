ea2f4d46937330f80285fd1c5be10d402f619380
Avala233 <avalianiii13@gmail.com>
2023-04-27 17:06:53 +0400

    day6

---
+++ day 6/lesson6.py
@@ -0,0 +1,77 @@
+#iputit sheitanet tqveni saxeli da gvari
+#inputshi terminali gamoitanet romel indeqsze aris xmovbnebi
+
+
+
+
+#davit avaliani
+#a-1
+#i-3
+#a-6
+#a-8
+#i-10
+#a-11
+#i-13
+
+# full_name = input("enter your name here: ")
+
+# i = 0
+# while i < len(full_name):
+#     if full_name[i] in "aeiou":
+#         print(str (i) + full_name[i] )
+#     i += 1
+
+# x = 0
+# for i in range (5):
+#     for j in range(4):
+#         for k in range(3):
+#             print(str(x)+"nika")
+#             x += 1       
+   
+# total_price = 0
+# for i in range(5):
+#     age_of_user = int(input("enter user age here: "))
+#     if age_of_user>=3:
+#         total_price += 100
+
+# print (total_price)
+# total_price = 0
+# i = 0
+# while i <5:
+#     age_of_user = int(input("enter your age here: "))
+#     if age_of_user>=3:
+#         total_price += 100
+#     i += 1
+# print(total_price)
+
+# n = int(input())
+# for x in range (1, n):
+#     if x % 2 == 1:
+#         if x % 3 == 0 and x % 5 == 0:
+#            print("sololearn")
+#         elif x % 3 == 0:
+#             print("solo")
+#         elif x % 5 == 0:
+#             print("learn")
+#         else:     
+#             print(x)
+
+#list = სია
+#სია იქმნება კვადრატული ფრჩხილით [  და მისი ელემენტები გამოყოფილია მძიმით , 
+
+# my_list = ["xinkali","mwvadi" , "lonbiani" , "khachapuri" , "wyali"]
+
+# prices = [2,20,15,10,15,2]
+
+# weird_list = ["nika","nika2","nika3", [1,2,3],"nika4",[4,5,6]]
+
+#print(my_list)
+ 
+# print(weird_list[3][1]) #დაპრინტავს მესამე პოზიციაში მყოფი [1,2.3] ის  პირველ პოზიციაში მყოფ 2
+#for food in (my_list):
+    #print(food)
+
+    #davaleba
+
+ #daiweros xinkali ghirs 2 lari 
+ #mwvadi girs 20 da aseeshemdeg
---
+++ day 6/lesson6hw.py
@@ -0,0 +1,17 @@
+# my_list = ["xinkali","mwvadi" , "lonbiani" , "khachapuri" , "wyali"]
+# prices = [2,20,15,10,15,2]
+
+#davaleba:
+  #daiweros xinkali ghirs 2 lari 
+  #mwvadi girs 20 da aseeshemdeg
+
+my_list = ["xinkali","mwvadi","lonbiani","khachapuri","wyali"]
+prices = [2,20,15,10,15,2]
+i = 0
+while i <5:
+    print(my_list[i] + " "+ "ghirs"+ "-" + str(prices[i]) + "lari")
+    i += 1
+
+task= "completed"
+if task == "completed":
+    print("EZ")
